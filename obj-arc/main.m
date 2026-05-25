4922d76c6ec42fd62a1f7f6acf0f73474391850e
Erik Solis <107826254+Soliez@users.noreply.github.com>
2026-05-25 00:40:53 -0400

    Initial Commit

---
+++ obj-arc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  obj-arc
+//
+//  Created by Erik Solis  on 2026-05-25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
