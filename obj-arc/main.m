f0a4ad0c55c0fc104116218b43db51696bc1139e
Erik Solis <107826254+Soliez@users.noreply.github.com>
2026-05-26 02:00:29 -0400

    Impelement helper functions 'isCodable' and 'requiresSecureCoding' for determining whether an object is keyed-archivable before attempting to serialize it.

--- obj-arc/main.m
+++ obj-arc/main.m
@@ -11,8 +11,25 @@
 #import <objc/runtime.h>
 
 
-BOOL isArchivable(id obj);
+BOOL isCodable(id obj)
+{
+    if (!obj){ return NO; }
+    Class class = object_getClass(obj);
+    return ([obj conformsToProtocol:@protocol(NSCoding)] | [obj conformsToProtocol:@protocol(NSSecureCoding)]) ? YES : NO;
+}
+
+BOOL requiresSecureCoding(id obj)
+{
+    if (!obj){ return NO; }
+    Class class = object_getClass(obj);
+    return (![obj conformsToProtocol:@protocol(NSCoding)] && [obj conformsToProtocol:@protocol(NSSecureCoding)]) ? YES : NO;
+}
+
+
+// TODO: Implement NSKeyedArchive load/dump operations
+
 NSData *DumpObject(id *obj);
+
 id LoadObject(NSData *archive);
 
 
